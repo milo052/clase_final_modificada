d79b05bf5bc45f245dba2678e88559a0457b79c0
milo052 <130940713+milo052@users.noreply.github.com>
2023-05-31 15:29:33 -0300

    modificacion_de_clasde_final
    modifico el carrusel y el js

---
+++ javascript.js
@@ -0,0 +1,24 @@
+var mayor = 18;
+alert("sos mayor de edad?")
+alert("¿que edad tienes?")
+
+
+var topping=prompt(" que edad tienes");
+var edad ; 
+var mayorDeEdad; 
+
+if (topping >= "18"){
+ alert("puedes entrar");
+}
+
+ else if (alert(
+   "entonces entras bajo tu responsabilidad"))
+
+console.log(precios)
+   var mayorDeEda= mayor+edad;
+
+
+
+
+
+alert("muchas gracias")
